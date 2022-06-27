1a42cbe62fb198536f7b9e2262c89ffd1749a038
viniciusokamoto <leo_kamoto@hotmail.com>
2022-06-26 22:07:12 -0500

    feat(MainActivity): set the view binding

--- Travely/app/src/main/java/com/example/travely/MainActivity.kt
+++ Travely/app/src/main/java/com/example/travely/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.travely
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.travely.databinding.ActivityMainBinding
 
+    //AppCompactActivity defines that the MainActivity go to behave as screen
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

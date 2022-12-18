bfca4132b5104a57211b21059d73fb1ed1444d1a
viniciusokamoto <leo_kamoto@hotmail.com>
2022-12-18 07:15:10 -0600

    feat(MainActivity): add layout

--- Motivation/app/src/main/java/com/example/motivation/MainActivity.kt
+++ Motivation/app/src/main/java/com/example/motivation/MainActivity.kt
@@ -2,16 +2,33 @@ package com.example.motivation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.motivation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBidining.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.hide()
+
+        binding.newPhraseButton.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View?) {
+        if(view?.id == R.id.newPhraseButton) {
+            var s = ""
+//            val phrase = binding.phraseText.text.toString()
+//            val author = binding.authorText.text.toString()
+//            val intent = Intent(this, PhraseActivity::class.java)
+//            intent.putExtra("phrase", phrase)
+//            intent.putExtra("author", author)
+//            startActivity(intent)
+        }
     }
 }

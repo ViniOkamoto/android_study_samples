1fa85cbac71e3788a02f82083138d07b8c73169e
viniciusokamoto <leo_kamoto@hotmail.com>
2022-12-18 04:29:58 -0600

    feat(MainActivity): add validation

--- Travely/app/src/main/java/com/example/travely/MainActivity.kt
+++ Travely/app/src/main/java/com/example/travely/MainActivity.kt
@@ -2,9 +2,10 @@ package com.example.travely
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.travely.databinding.ActivityMainBinding
 
-    //AppCompactActivity defines that the MainActivity go to behave as screen
+//AppCompactActivity defines that the MainActivity go to behave as screen
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
@@ -13,20 +14,28 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        fun calculate() {
+
+        binding.buttonCalculate.setOnClickListener {
+            calculate()
+        }
+
+
+    }
+
+    private fun validate(): Boolean {
+        return (binding.editGasCost.text.toString() != "" && binding.editTravelDistance.text.toString() != "" && binding.editVehicleAutonomy.text.toString() != "" && binding.editVehicleAutonomy.text.toString().toFloat() != 0f)
+    }
+
+    private fun calculate() {
+        if(validate()){
             val distance = binding.editTravelDistance.text.toString().toInt()
             val price = binding.editGasCost.text.toString().toInt()
             val autonomy = binding.editVehicleAutonomy.text.toString().toInt()
 
             val result = (distance * price) / autonomy
-            binding.textTotalCost.text = "R$ $result"
+            binding.textTotalCost.text = "R$ ${"%.2f".format(result)}"
+            return;
         }
-
-        binding.buttonCalculate.setOnClickListener() {
-            calculate()
-        }
-
-
-
+        Toast.makeText(this, R.string.validate_error_all_fields, Toast.LENGTH_SHORT).show()
     }
 }

276b3afdfa75db36ec6d652a519f9f076751406c
viniciusokamoto <leo_kamoto@hotmail.com>
2022-12-17 20:44:21 -0600

    feat(Travely): activity binding

--- Travely/app/src/main/java/com/example/travely/MainActivity.kt
+++ Travely/app/src/main/java/com/example/travely/MainActivity.kt
@@ -12,5 +12,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        fun calculate() {
+            val distance = binding.editTravelDistance.text.toString().toInt()
+            val price = binding.editGasCost.text.toString().toInt()
+            val autonomy = binding.editVehicleAutonomy.text.toString().toInt()
+
+            val result = (distance * price) / autonomy
+            binding.textTotalCost.text = "R$ $result"
+        }
+
+        binding.buttonCalculate.setOnClickListener() {
+            calculate()
+        }
+
+
+
     }
 }
